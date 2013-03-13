84708d3dc960e44711f05e94f1014f4104242cbe
Septian Rahman <tianboyandmikroskil@gmail.com>
2013-03-13 23:27:42 +0700

    newly

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='myblog',
+    version='1.0.1',
+    description='MyBlog - OpenShift App',
+    author='Kamal Mustafa',
+    author_email='kamal.mustafa@gmail.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=[
+        'Django>=1.3',
+        'mezzanine',
+        'Pillow',
+    ],
+)
