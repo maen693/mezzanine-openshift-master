7b931fea7168137de47d7626f752588d012008b1
Andrew <soho@illucent.info>
2013-08-20 18:08:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setup(
     author_email='kamal.mustafa@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=[
-        'Django>=1.3',
-        'mezzanine',
+        'Django>=1.5.2',
+        'mezzanine>=1.4.10',
         'Pillow',
+        'django_compressor==1.3'
     ],
 )
